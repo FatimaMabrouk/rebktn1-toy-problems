33967851101d26141c051c93068a931aa23af335
BigBBaker <abobkerelaghel@gmail.com>
2019-11-25 09:15:08 +0100

    25/11

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,15 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+ string = 'AACBDB'
+arr = {};
+
+counter = [];
+  for (var i = 0; i < string.length; i++) {
+      arr[string.charCodeAt(i)-65]++;  ;
+  }
+arr
+
 };
+
+//uncomplet answer
