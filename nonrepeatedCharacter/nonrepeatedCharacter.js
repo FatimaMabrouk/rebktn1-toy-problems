b3d954a6ed4a8d5abd64af916a84564101e3b53c
waj949 <wajdihamrouni1993@gmail.com>
2019-11-25 10:12:22 +0100

    toyproblem

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,12 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+  if(string.length === 0){
+  	return 'all characters are repeated';
+  }
+  if(string.indexOf(string[0] === -1)){
+  	return string[0];
+  }
+   return firstNonRepeatedCharacter(string.slice(1))
+
 };
