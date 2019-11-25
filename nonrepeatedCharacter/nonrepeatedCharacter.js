316232c5adee89b1ab9f2822d5cdf10a68ee574e
bilel <hb.bilel@gmail.com>
2019-11-25 09:06:34 +0100

    done !

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,22 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
-};
+  obj = {}
+  var arr = []
+  var count = 0
+  for (var i = 0; i < string.length; i++) {
+    var test = string[i]
+    if (!obj[test]) {
+      obj[test] = true
+      arr.push(test)
+    } else {
+      if (arr[count] === test) {
+      count += 1
+      }
+    }
+
+  }
+  return arr[count]
+}
+
+
