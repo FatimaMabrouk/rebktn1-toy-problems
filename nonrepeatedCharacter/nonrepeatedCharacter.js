01abd82c17d2d2c493acab83c938b240adb65f20
essam <esamfarg91@gmail.com>
2019-11-25 09:07:35 +0100

    good one

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,15 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  var doubled = [];
+  var single = [];
+    for (var i = 0; i < string.length; i++) {
+  		if(single.indexOf(string[i]) !== - 1) { 
+  			doubled.push(string[i]);
+  			single.splice(single.indexOf(string[i]), 1);
+  		}else if(doubled.indexOf(string[i]) === -1){
+  			single.push(string[i]);
+  		}
+  	}
+  	return single[0];
 };
