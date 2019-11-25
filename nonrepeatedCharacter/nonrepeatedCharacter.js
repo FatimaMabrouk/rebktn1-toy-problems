957b951c587392426c9ea3180c89a8671e2fbf3a
rue-elwahishi <rubawahishi@gmail.com>
2019-11-25 10:13:25 +0100

    incomplete, missing one tiny step

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,27 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+	str = '';
+   var counter = [];
+   for (var i = 0; i < string.length; i++){
+   	     counter.push(1);
+   }
+   var arr = string.split('')
+   for (var j = 0;  j < string.length; j++){
+   	   str = str + counter[arr.indexOf(string[j])]++
+       if(str === 1){
+       	 str = string[j]
+       }
+
+       return str; ;
+   //    var str = '';
+   //    var arr = string.split('');
+   //    var counter = 0;
+   // for(var i= 0; i < arr.length; i++)
+   	
+
+   //    if(counter === 1){
+   //    	return arr[i]
+   //    }
+   //    return str + firstNonRepeatedCharacter(string, counter - 1 )
 };
