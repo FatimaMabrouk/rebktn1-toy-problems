07b58809d9956567b874b8db6a602fb201418733
Amera Ayad <amera.ayad.it@gmail.com>
2019-11-27 09:58:12 +0100

    amera

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -4,7 +4,7 @@
  * order that they appeared in `a`. Remember to skip spaces and characters you
  * have already encountered!
  *
- * Example: commonCharacters('acexivou', 'aegihobu')
+ * Example: commonCharacters('aacexivou', 'aegihobu')
  * Returns: 'aeiou'
  *
  * Extra credit: Extend your function to handle more than two input strings.
@@ -13,5 +13,27 @@
 
 
 var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
-};
+  var result = [];
+  for(i = 0; i < string1.length; i++){
+    if(string2.includes(string1[i]) && !result.includes(string1[i])) {
+      result.push(string1[i]);
+    }
+  }
+  return result.join('');
+}
+
+///commonCharacters2 this is for using more than 2 args but it doesnt work
+var commonCharacters2 = function(...rest) {
+  var result = [];
+  while(rest.length > 0){
+    if(!!rest[1]){
+      result.push(commonCharacters(rest[0], result.join('')));
+      rest.splice(0, 1);
+    } else {
+      result.push(commonCharacters(rest[0], rest[1]));
+    rest.splice(0, 1);
+    }
+    
+  }
+  return result.join('');
+}
