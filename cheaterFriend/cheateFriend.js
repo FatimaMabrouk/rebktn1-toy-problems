3de963e93955865081a288e1d0c37fd47b08bae6
Amera Ayad <amera.ayad.it@gmail.com>
2019-11-28 09:44:01 +0100

    solved

--- cheaterFriend/cheateFriend.js
+++ cheaterFriend/cheateFriend.js
@@ -1,10 +1,14 @@
 
 //     A friend of mine takes a sequence of numbers from 1 to n (where n > 0).
 //     Within that sequence, he chooses two numbers, a and b.
-//     He says that the product of a and b should be equal to the sum of all numbers in the sequence, excluding a and b.
+
+//     He says that the product of a and b should be equal to
+// the sum of all numbers in the sequence, excluding a and b.
+
 //     Given a number n, could you tell me the numbers he excluded from the sequence?
 
-// The function takes the parameter: n (n is always strictly greater than 0) and returns an array or a string (depending on the language) of the form:
+// The function takes the parameter: n (n is always strictly greater than 0) 
+//and returns an array or a string (depending on the language) of the form:
 
 // [(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or or [{a, b}, ...]
 
@@ -12,8 +16,37 @@
 
 // [(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or ...will be sorted in increasing order of the "a".
 
-// It happens that there are several possible (a, b). The function returns an empty array (or an empty string) if no possible numbers are found which will prove that my friend has not told the truth! (Go: in this case return nil).
+// It happens that there are several possible (a, b).
+// The function returns an empty 
+//array (or an empty string) if no possible numbers are found which will prove that 
+//my friend has not told the truth! (Go: in this case return nil).
+
 //
 //
 // Example:
 // removNb(26) should return [ {15, 21}, {21, 15} ]
+
+let removNb = (n) => {
+    let numbers = [];
+    let sum = 0;
+    let result = [];
+
+    while(n > 0){
+        numbers.push(n);
+        n--;
+    }
+
+    numbers.forEach((number) => {
+        sum += number;
+    });
+
+    for(let i = 0; i < numbers.length; i++){
+        for(let j = 1; j < numbers.length; j++){       
+            if(numbers[i] * numbers[j] === (sum - numbers[i] - numbers[j]) ){
+                result.push([numbers[i], numbers[j]]);
+            }
+        }
+    }
+    
+    return JSON.stringify(result);
+}
