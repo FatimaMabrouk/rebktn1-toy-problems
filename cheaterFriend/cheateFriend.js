0cdb3dd9e8f0a924c54ae61cddac9b18a8d37244
Anes <marzoukianas@gmail.com>
2019-11-28 09:54:52 +0100

    ggffg

--- cheaterFriend/cheateFriend.js
+++ cheaterFriend/cheateFriend.js
@@ -12,8 +12,56 @@
 
 // [(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or ...will be sorted in increasing order of the "a".
 
-// It happens that there are several possible (a, b). The function returns an empty array (or an empty string) if no possible numbers are found which will prove that my friend has not told the truth! (Go: in this case return nil).
+// It happens that there are several possible (a, b). The function returns an empty array (or an empty string) if no possible numbers are found which 
+//will prove that my friend has not told the truth! (Go: in this case return nil).
 //
 //
 // Example:
 // removNb(26) should return [ {15, 21}, {21, 15} ]
+var remobNB = function (n) {
+		var sumOfNum;
+		var array = [];
+	//creates an array frorm n [1...n]
+		while (!!n) {
+			array.push(n);
+			n--;
+		}
+
+		//check the sum of all numbers
+		 function sum (array) {
+			return array.reduce(function(acc, element) {
+			return acc += element;
+			})
+		}
+		sumOfNum = sum(array);
+		var newArr = [];
+		function prod (elem) {
+			for ( var i = 0; i < array.length; i++ ) {
+				if ( ( array[i] * elm )  === sumOfNum ) {
+					return newArr.push(array[i], elm)
+				} 
+			}
+			prod(array[array.lenght])
+		}
+		//if ( array.includes ( sumOfNum / Math.max(...array) ) )
+
+}
+
+
+
+/*
+var makeArray = function(n) {
+	var array = [];
+	while (!!n) {
+		array.push(n);
+		n--;
+	}
+	return array.sort();
+}
+
+var sum = function(array) {
+	return array.reduce(function(acc, element) {
+	return acc += element;
+	})
+}
+*/
