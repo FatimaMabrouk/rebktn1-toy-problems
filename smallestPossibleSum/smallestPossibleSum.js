ff09fd4bcbe654b7208c9d60eef6a6454470d7fc
Anes <marzoukianas@gmail.com>
2019-11-29 09:53:56 +0100

    tP

--- smallestPossibleSum/smallestPossibleSum.js
+++ smallestPossibleSum/smallestPossibleSum.js
@@ -9,7 +9,7 @@
 // X_1 = [6, 9, 12] # -> X_1[2] = X[2] - X[1] = 21 - 9
 // X_2 = [6, 9, 6]  # -> X_2[2] = X_1[2] - X_1[0] = 12 - 6
 // X_3 = [6, 3, 6]  # -> X_3[1] = X_2[1] - X_2[0] = 9 - 6
-// X_4 = [6, 3, 3]  # -> X_4[2] = X_3[2] - X_3[1] = 6 - 3
+// X_4 = [6, 3, 3]  # -> X_4[2] = X_3[2] - X_3[1] = 6  - 3
 // X_5 = [3, 3, 3]  # -> X_5[1] = X_4[0] - X_4[1] = 6 - 3
 
 // The returning output is the sum of the final transformation (here 9).
@@ -28,3 +28,39 @@
 // Additional notes:
 
 // There are performance tests consisted of very big numbers and arrays of size at least 30000. Please write an efficient algorithm to prevent timeout.
+
+ var smallestSum = function(array) {
+	var sortedArray;
+	//check for the array's length
+	if( array.length >= 30000 ) {
+		return;
+	}
+//sort the array (and reverse it)
+	//array = array.sort();
+
+		//declare a new array to hold the new value of .map
+	for (var i = 0; i < array.length; i++) {
+		if(array[i] > array[i + 1]) {
+		// console.log(array)
+		while ( array[i] > array[i + 1] ) {
+			array[i] = array[i] - array[i + 1];
+			//array.sort();
+			console.log(array)
+		}
+        } else{array.}
+		
+	}
+	return sortedArray;
+
+}
+// var greaterThan = function( array ) {
+
+// 	array.map( function(element, index) {
+// 		while( element > element + 1) {
+// 			element = element - ( element + 1 );
+// 			array.sort();
+// 		}
+
+// 	})
+
+// }
