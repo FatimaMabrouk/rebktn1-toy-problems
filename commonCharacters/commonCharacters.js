3ec476a6c03c166eb66ddd4cbd8d7f91e6f663b7
waj949 <wajdihamrouni1993@gmail.com>
2019-11-29 09:59:14 +0100

    smallestSum

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -12,6 +12,27 @@
 
 
 
-var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
-};
+v
+
+var commonCharacters = function(...arguments){
+	var acc = '';
+	var obj ={};
+	// i have to check if a character from string one exist in the all arguemnts
+	var str1 = arguments[0]; // first string
+	for(var j =0; j < str1.length; j++){ // loop throw first string
+
+	var check = true; // check acc
+	for(var i = 1; i < arguments.length; i++){ // loop throw arguments and check
+		check = check && arguments[i].includes(str1[j])
+	}
+	if(check){
+		acc = acc + str1[j]; // add to the acc
+	}
+  }
+   // remove duplications
+  for(var i = 0; i < acc.length; i++){
+   obj[acc[i]] = i;
+  }
+
+  return Object.keys(obj).join('');
+} 
