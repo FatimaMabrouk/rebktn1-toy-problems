9e6b7e7a3b036ea2c22c5eadf0fa7409363659c0
bilel <hb.bilel@gmail.com>
2019-12-03 09:46:22 +0100

    unfinished

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -10,6 +10,31 @@
  * console.log(index); // null
  */
 
-var binarySearch = function (array, target) {
-};
+var binarySearch = function (array, target, index) {
+     index = index || undefined;
 
+        var middle = Math.floor(Math.floor(array.length) / 2 )
+        console.log(middle)
+
+        if (target > array[array.length - 1] || target < array[0] || array.length === 0){
+            return null
+        }
+        if (target === array[middle]) {
+            return index
+        }
+        if (target > array[middle]) {
+            var tab = array.slice(middle)
+            console.log(tab)
+            index = middle + 1 
+            console.log(index)
+            return binarySearch(tab, target, index)
+        }
+
+        if (target < array[middle]) {
+            var tab = array.slice(0, middle + 1)
+            index = middle - 1  
+            console.log(tab)
+            console.log(index)
+            return binarySearch(tab, target,index)
+        }
+    };
