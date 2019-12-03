ba5e9259c3b2324fd3b52200414b5acf9e448c5e
amineslaimia <medamineslaimia@gmail.com>
2019-12-03 09:13:52 +0100

    toys

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,20 @@
  */
 
 var binarySearch = function (array, target) {
+var mid = 0
+  if (array.length===1){
+      return array
+  }
+    if(array.length %2 === 0){
+        mid= (array.length /2)-1
+    }else if (array.length %2 === 1){
+        mid= array.length -1/2
+    }
+    if(array[mid]>target){
+        binarySearch(array.splice(0,mid+1),target)
+    }else{
+        binarySearch(array.splice(mid,array.length-1),target)
+    }
+    return array
 };
 
