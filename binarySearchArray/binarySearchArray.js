20faf27f1f535717157b28d67d5b33aa55e649cb
AbdoAlabed <henry_3030@icloud.com>
2019-12-03 00:39:47 -0800

    logical work

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,29 @@
  */
 
 var binarySearch = function (array, target) {
+    var ta = target;
+	var index = Math.floor(array.length/2);
+	if(index === 1){
+		if(ta === array[index]){
+			return index;
+		}
+		else if(ta === array[index-1]){
+			return index;
+		}
+		else{
+			return null;
+		}
+	}
+	if(ta === array[index]){
+		return index;
+	}
+	else if(ta > array[index]){
+		array =array.slice(index+1)
+		binarySearch(array,ta)
+	}
+	else if(ta< array[index]){
+		array =array.slice(0,index)
+		binarySearch(array,ta)
+	}
 };
 
