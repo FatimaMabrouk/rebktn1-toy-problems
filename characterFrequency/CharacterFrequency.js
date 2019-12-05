c904b0551c7ad375ba529844bf88645c263fa908
AbdoAlabed <henry_3030@icloud.com>
2019-12-05 02:11:27 -0800

    almost done

--- characterFrequency/CharacterFrequency.js
+++ characterFrequency/CharacterFrequency.js
@@ -11,3 +11,43 @@ characterFrequency("mississippi")  -->  [ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ 
 
 characterFrequency("") -->  [ ]
 */
+
+function characterFrequency(str){
+    var arr = [];
+    var newArr = [];
+
+	for (var i = 0; i < str.length; i++) {
+		var count = 0;
+
+		for (var j = 0; j < str.length; j++) {
+
+			   if(str[i] === str[j]){
+               count++;
+  
+			}
+		}
+		
+		arr.push([str[i],count])
+	}
+	// for (var i = 0; i < arr.length-1; i++) {
+	// 	if(arr[i] === arr[i+1]){
+	// 		arr = arr.slice(i+1)
+	// 	}
+	// }
+	
+	var temp = 0;
+    for (var i = 0; i < arr.length; i++) {
+        
+        for (var j = i+1; j < arr.length; j++) {
+
+    		if(arr[i][1] < arr[j][1]){
+               temp = arr[i][1];
+               arr[i][1] = arr[j][1];
+               arr[j][1] = temp;
+    		}
+        	
+        }
+    }
+
+	return arr;
+}
