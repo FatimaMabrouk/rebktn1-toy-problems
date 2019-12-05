381baa5d077966d5fb6f608456b5d5d2b585eaae
AdamMomen <adammuman81@gmail.com>
2019-12-05 09:58:20 +0100

    Progress

--- characterFrequency/CharacterFrequency.js
+++ characterFrequency/CharacterFrequency.js
@@ -11,3 +11,24 @@ characterFrequency("mississippi")  -->  [ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ 
 
 characterFrequency("") -->  [ ]
 */
+var characterFrequency = function (string) {
+debugger;
+var  ResultArr;
+ ResultArr  = string.slice().split('').map(function (element, index, array) {
+		var counter  = 1;
+		for (var i = 0; i < array.length; i++) {
+			if (element === array[i]) {
+				array.splice(i,1);
+				counter++;
+			}	
+		}
+			if(array.length===1) {return [array[0], 1]} 
+		array.splice(0,1);
+	
+		return [element, counter]
+	});
+return ResultArr.forEach(function (val, index) {
+//bubble sort
+});
+}
+characterFrequency("aaabbc");
