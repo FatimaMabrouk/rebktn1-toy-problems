83f866e4f2c8888d12b5e54148693bcc806c4d49
essam <esamfarg91@gmail.com>
2019-12-05 09:33:18 +0100

    amazing

--- characterFrequency/CharacterFrequency.js
+++ characterFrequency/CharacterFrequency.js
@@ -11,3 +11,33 @@ characterFrequency("mississippi")  -->  [ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ 
 
 characterFrequency("") -->  [ ]
 */
+
+function characterFrequency(str) {
+	var obj = {};
+	var result = [];
+
+	for (var i = 0; i < str.length; i++) {
+		if(!obj.hasOwnProperty(str[i])){
+			obj[str[i]] = 1;
+		}else {
+			obj[str[i]] += 1;
+		}
+	}
+
+	var keys = Object.keys(obj);
+
+	for (var i = 0; i < keys.length; i++) {
+		var max = keys[i];
+		var ind = i
+		for (var j = 0; j < keys.length; j++) {
+			if(obj[keys[j]] > obj[max]) {
+				max = keys[j];
+				ind = j;
+			}
+		}
+		result.push([max, obj[max]]);
+		obj[max] = 0;
+	}
+
+	return result;
+}
