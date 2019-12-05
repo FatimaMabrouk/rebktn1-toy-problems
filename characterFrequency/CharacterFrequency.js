1e76571651e790bb083a11ac9d380d8ecfe2b260
B.Maher <bouzid.meher@gmail.com>
2019-12-05 09:36:40 +0100

    charachter frequency

--- characterFrequency/CharacterFrequency.js
+++ characterFrequency/CharacterFrequency.js
@@ -11,3 +11,23 @@ characterFrequency("mississippi")  -->  [ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ 
 
 characterFrequency("") -->  [ ]
 */
+
+var characterFrequency = (str) => {
+    var obj = new Map(); //create an object to store the chars and there occurance frequency
+    var result = []; //create an array to store the results in
+    //loop over the string
+    for (var i = 0; i < str.length; i++) {
+        //check if the char is already in the object
+        if (obj.get(str[i])) {
+            //increment by 1 the frequency
+            obj.set(str[i], obj.get(str[i]) + 1)
+        } else {
+            //add the char to the object
+            obj.set(str[i], 1);
+        }
+    }
+    //return an array of array sorted out of the obj
+    return Array.from(obj).sort((a, b) => {
+        return b[1] - a[1]
+    })
+}
