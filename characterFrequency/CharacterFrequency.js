5f2c189ae9ffa8265731ef5a455669d3342aa526
Anes <56113177+Anes-Marzooki@users.noreply.github.com>
2019-12-05 15:36:22 +0100

    Refactored
    ...

--- characterFrequency/CharacterFrequency.js
+++ characterFrequency/CharacterFrequency.js
@@ -1,56 +1,22 @@
-/*
-Character Frequency
-Write a function call characterFrequency that takes as its input a string and returns an array
-of arrays as shown below sorted in descending order by frequency
-and then by ascending order by character.
+function characterFrequency(str){
+    var str1 = Array.from(str).sort().join(''); //  "iiiimppssss"
+    //var str1 = str.split("").sort().join("");
+    var result = [];
 
-
-characterFrequency("aaabbc") -->    [ [ "a", 3 ], [ "b", 2 ], [ "c", 1 ] ]
-
-characterFrequency("mississippi")  -->  [ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ "m", 1 ] ]
-
-characterFrequency("") -->  [ ]
-*/
-
-
-// ?? array.sort( function() )
-
-
-function characterFrequency(str) {
-
-	var array = Array.from(arguments).sort(); // ["i", "i", "i", "i", "m", "p", "p", "s", "s", "s", "s"]
-	var result = [];
-	var empty = [];
-	var count = 0;
-	var elem = array[0];
-	var arr = array;
-
-/*
-//divide the array
-//
-
-*/
+    var rec = function(str1) {
+        if(str1 === "") {return;}
+        var lIO = str1.lastIndexOf(str1[0]) + 1; //3 + 1
+        result.push([str1[0],lIO]);
+        return rec(str1.substr(lIO)); //subtr(numb)
+    }
+    rec(str1);
 
 
 
-
-
-
-
-	while(arr.length >= 0) {                                //!!!!!!!!!!!!!!!!!CRASHES!!!!!!!!!!!!!!!!!!11
-			
-			if( elem === arr[0] ) { // p === p
-				count++;
-				arr.shift();
-			} else {
-				result.push(elem, count);
-				empty.push(result); // [[i,3],[m,1]
-
-				elem = array[0]; // p
-				count = 1;
-			}
-	}
-	return empty;
+    // while(str1 !== '') {
+    //     var lIO = str1.lastIndexOf(str1[0]) + 1; //3 + 1
+    //     result.push([str1[0], lIO]);
+    //     str1.substr(lIO); //mppssss
+    // }
+    return result.sort(function(a,b){return b[1]-a[1]});
 }
-
-
