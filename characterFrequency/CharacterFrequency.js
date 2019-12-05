ad633559cf9d9036f615042222350d7f673f062d
Anes <marzoukianas@gmail.com>
2019-12-05 09:48:08 +0100

    sorted frequency

--- characterFrequency/CharacterFrequency.js
+++ characterFrequency/CharacterFrequency.js
@@ -11,3 +11,46 @@ characterFrequency("mississippi")  -->  [ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ 
 
 characterFrequency("") -->  [ ]
 */
+
+
+// ?? array.sort( function() )
+
+
+function characterFrequency(str) {
+
+	var array = Array.from(arguments).sort(); // ["i", "i", "i", "i", "m", "p", "p", "s", "s", "s", "s"]
+	var result = [];
+	var empty = [];
+	var count = 0;
+	var elem = array[0];
+	var arr = array;
+
+/*
+//divide the array
+//
+
+*/
+
+
+
+
+
+
+
+	while(arr.length >= 0) {                                //!!!!!!!!!!!!!!!!!CRASHES!!!!!!!!!!!!!!!!!!11
+			
+			if( elem === arr[0] ) { // p === p
+				count++;
+				arr.shift();
+			} else {
+				result.push(elem, count);
+				empty.push(result); // [[i,3],[m,1]
+
+				elem = array[0]; // p
+				count = 1;
+			}
+	}
+	return empty;
+}
+
+
