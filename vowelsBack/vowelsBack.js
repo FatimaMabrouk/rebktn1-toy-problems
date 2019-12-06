b5713e99b55ad8f68c6dbaaf5ef65a147d711e1d
Hamza <hamzadanden2@gmail.com>
2019-12-06 09:45:10 +0100

    done

--- vowelsBack/vowelsBack.js
+++ vowelsBack/vowelsBack.js
@@ -11,3 +11,47 @@
 // If a moved letter becomes 'c', 'o', 'd' or 'e', revert it back to it's original value.
 
 // Provided string will always be lower case, won't be empty and will have no special characters.
+
+
+var vowelsBack = function(str) {
+	var result = '';
+	for(var i = 0; i < str.length; i++) {
+		var test = ''
+		if(str[i] === 'c' || str[i] === 'o') {
+			test = String.fromCharCode(str.charCodeAt(i) - 1);
+		} else if(str[i] === 'd') {
+			test = String.fromCharCode(str.charCodeAt(i) - 3);
+		} else if(str[i] === 'e') {
+			test = String.fromCharCode(str.charCodeAt(i) - 4);
+		} else if(['a', 'e', 'i', 'o', 'u'].includes(str[i])){
+
+		if(str.charCodeAt(i) - 5 < 97) {
+
+			var count = 97 - (str.charCodeAt(i) - 5);
+			test = String.fromCharCode(122 - count);
+
+		} else {
+			test = String.fromCharCode(str.charCodeAt(i) - 5);
+		}
+
+
+		}else {
+
+			if(str.charCodeAt(i) + 9 > 122){
+				var count = str.charCodeAt(i) + 9 - 122;
+				test = String.fromCharCode(97 + count);
+			}else {
+				test = String.fromCharCode(str.charCodeAt(i) + 9);	
+			}
+			
+		}
+		if (['c', 'o', 'd','e'].includes(test)) {
+			result += str[i]
+		} else {
+			result += test
+		}
+
+	}
+	console.log(result);
+}
+
