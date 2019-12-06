06a804f869a8f85b898e8919ee9e0284f3d42220
essam <esamfarg91@gmail.com>
2019-12-06 10:01:57 +0100

    almost done

--- vowelsBack/vowelsBack.js
+++ vowelsBack/vowelsBack.js
@@ -2,7 +2,8 @@
 
 // Move consonants forward 9 places through the alphabet. If they pass 'z', start again at 'a'.
 
-// Move vowels back 5 places through the alphabet. If they pass 'a', start again at 'z'. For our Polish friends this kata does not count 'y' as a vowel.
+// Move vowels back 5 places through the alphabet. If they pass 'a', start again at 'z'.
+// For our Polish friends this kata does not count 'y' as a vowel.
 
 // Exceptions:
 
@@ -11,3 +12,48 @@
 // If a moved letter becomes 'c', 'o', 'd' or 'e', revert it back to it's original value.
 
 // Provided string will always be lower case, won't be empty and will have no special characters.
+
+function vowelBack(str) {
+	str = str.split('');
+	arr = ' abcdefghijklmnopqrstuvwxyz'.split('');
+	excepObj = {c: 1, o: 1, d: 3, e: 4};
+	excepRes = ["c", "o", "d", "e"];
+	vowelLetter = ['a', 'i', 'u'];
+	arr[0] = undefined;
+	for (var i = 1; i < str.length; i++) {
+		// var changed = false;
+		var arrIndex = arr.indexOf(str[i])
+		if(arrIndex - 5 <= 0){
+			var exceptionValVowel =excepRes.indexOf(arr[arrIndex - 5 + 26])
+		}else {
+			var exceptionValVowel =excepRes.indexOf(arr[arrIndex - 5])
+		}
+
+		if(arrIndex + 9 > 26){
+			var exceptionValCons = excepRes.indexOf(arr[arrIndex + 9 - 26])
+		}else {
+			var exceptionValCons = excepRes.indexOf(arr[arrIndex + 9 ])
+		}
+		if(excepObj[str[i]]){
+			if(excepRes.indexOf(arr[arr.indexOf(str[i]) - excepObj[str[i]]]) === -1){
+				str[i] = arr[arrIndex - excepObj[str[i]]]
+			}
+			// changed = true;
+		}else if(vowelLetter.indexOf(str[i]) != -1) {
+			if(exceptionValVowel) {
+				str[i] = arr[arrIndex - 5];
+			}
+		}else {
+			if(exceptionValCons != -1){
+				str[i] = arr[arrIndex + 9]
+			}
+		}
+	}
+	return str.join('')
+
+}
+
+// Test.describe("Example tests",_=>{
+// Test.assertSimilar(vowelBack("testcase"), "tabtbvba");
+// Test.assertSimilar(vowelBack("codewars"), "bnaafvab");
+// Test.assertSimilar(vowelBack("exampletesthere"), "agvvyuatabtqaaa");
