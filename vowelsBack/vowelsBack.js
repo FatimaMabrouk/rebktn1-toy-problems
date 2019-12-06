f8a6c9528c1729b7b2390c61bed4448e4c4f67df
Anes <marzoukianas@gmail.com>
2019-12-06 09:51:11 +0100

    PLatin

--- vowelsBack/vowelsBack.js
+++ vowelsBack/vowelsBack.js
@@ -10,4 +10,40 @@
 
 // If a moved letter becomes 'c', 'o', 'd' or 'e', revert it back to it's original value.
 
-// Provided string will always be lower case, won't be empty and will have no special characters.
+// Provided string will always be lower case, won't be empty and will have no special characters.	
+
+var alph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var consonant = ["b", "c", "d", "f", "g", "h",  "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+var vowels = ["a", "e", "i", "o", "u"];
+
+
+
+
+function jargon(str) {
+	var result = '';
+	var trans;
+	str = str.toLowerCase();
+
+	for(var i = 0; i < str.length; i++) {
+		if( consonant.includes(str[i]) ) { 
+			
+				if( (alph.indexOf( str[i] ) +9) < alph.length ) {
+					str += alph[alph.indexOf( str[i] )+9];
+				} else {
+					var countC = 9 - ( (alph.length-1) - alph.indexOf( str[i] ) ); //r --> a
+					str+= alph[countC];
+				}
+
+		}else {
+			if( (alph.indexOf(str[i]) -5) >= 0) {
+console.log('hi')
+				str += alph[alph.indexOf( str[i] )-5];
+			} else {
+				var countV = 5 -  ( alph.indexOf(str[i]) );
+				str+= alph[alph.lenght - countV];
+			}
+		}
+	}
+return result;
+
+}
