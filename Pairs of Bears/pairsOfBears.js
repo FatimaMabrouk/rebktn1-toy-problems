c9a16719ab63f3c6ddfcdd60787065f33b56016e
B.Maher <bouzid.meher@gmail.com>
2019-12-07 09:18:27 +0100

    Pairs of bears done

--- Pairs of Bears/pairsOfBears.js
+++ Pairs of Bears/pairsOfBears.js
@@ -20,3 +20,24 @@ x will always be a positive integer, and s will never be empty
 
 
 */
+
+function findMatingPairs(nbr, string) {
+    let result = [""];
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === 'B' || string[i] === '8') {
+            //make a copy of the two letters
+            let str = string.slice(i, i + 2);
+            //set a condition to check if any letter before or after the current pair of letters is equal to B or 8
+            let condition = string[i + 2] !== "B" && string[i + 2] !== "8" && string[i - 1] !== "B" && string[i - 1] !== "8";
+            if ((str === "8B" || str === "B8") && condition) {
+                result[0] += str;
+            }
+        }
+    }
+    if (result[0].length / 2 >= nbr) {
+        result.push(true);
+    } else {
+        result.push(false);
+    }
+    return result;
+}
