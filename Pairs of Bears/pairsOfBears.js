0e99f7963f90e50824a1dc64fdbe1a5842caa294
bilel <hb.bilel@gmail.com>
2019-12-07 09:40:57 +0100

    not done

--- Pairs of Bears/pairsOfBears.js
+++ Pairs of Bears/pairsOfBears.js
@@ -1,10 +1,13 @@
 /*
 
-In order to prove it's success and gain funding, the wilderness zoo needs to prove to environmentalists that it has x number of mating pairs of bears.
+In order to prove it's success and gain funding, the wilderness zoo needs to prove to environmentalists 
+that it has x number of mating pairs of bears.
 
-You must check within string (s) to fid all of the mating pairs, and return a string containing only them. Line them up for inspection.
+You must check within string (s) to fid all of the mating pairs, and return a string containing only them. 
+Line them up for inspection.
 
-Rules: Bears are either 'B' (male) or '8' (female), Bears must be together in male/female pairs 'B8' or '8B', Mating pairs must involve two distinct bears each ('B8B' may look fun, but does not count as two pairs).
+Rules: Bears are either 'B' (male) or '8' (female), Bears must be together in male/female pairs 'B8' or '8B',
+ Mating pairs must involve two distinct bears each ('B8B' may look fun, but does not count as two pairs).
 
 Return an array containing a string of only the mating pairs available. e.g:
 
@@ -20,3 +23,33 @@ x will always be a positive integer, and s will never be empty
 
 
 */
+
+function pairOfBears(nb,string){
+	var result = []
+	var test1 = "B8"
+	var test2 = "8B"
+	var ch =""
+	var count = 0
+	var i = 0
+	while(i < string.length){
+		console.log("test")
+		var index = string.toUpperCase().indexOf(test1) || string.toUpperCase().indexOf(test2)
+		if(index !== -1 ){
+			var el = string.slice(index,index+2)
+			var string = string.replace(el,'')	
+			string = string.replace(el,'')
+			count++
+		}
+		ch += el
+		i++
+
+	}
+	result.push(ch)
+	result.push(count>nb)
+
+	return result
+}
+
+function remove(str, start, n) {
+    return str.substr(0, n) + str.substr(start + n);
+}
--- vowelsBack/vowelsBack.js
+++ vowelsBack/vowelsBack.js
@@ -15,8 +15,67 @@
 
 function vowels(string){
 	var reg = /[!@#$%^&*(),.?":{}|<> ]/g
-	var tab = string.split(reg)	
-	var newstring.replace(reg,''))
+	var vowelsCh = /[eui]/g
+	var coTest = /[co]/g
+	var azTest = /[az]/g
+	var movedLetterTest = /[code]/g
+	var newString = string.replace(reg,'')
+	var tab = newString.split('')
 	//console.log(string)
-	return tab
+	var regexVowels = RegExp(vowelsCh)
+	var result =""
+	for(var i = 0 ; i <tab.length ; i++){
+		/*if(tab[i]=== "z" ){
+			result += "a"
+		}
+		if(tab[i]=== "a" ){
+			result += "z"
+		}*/
+		if(tab[i]=== "d" ){
+			var code = tab[i].charCodeAt() -3
+			var ch = String.fromCharCode(code);
+			result += ch
+		}
+		if(tab[i]=== "e" ){
+			var code = tab[i].charCodeAt() - 4
+			var ch = String.fromCharCode(code);
+			if(!check){
+				result += ch	
+			}else{
+				result += tab[i]
+			}
+			
+		}
+		if(tab[i] === "c" || tab[i] === "o" ){
+			var code = tab[i].charCodeAt() + 1
+			var ch = String.fromCharCode(code);
+			if(!check){
+				result += ch	
+			}else{
+				result += tab[i]
+			}
+		}
+		if(!regexVowels.test(tab[i])){
+			var code = tab[i].charCodeAt() + 9
+			var ch = String.fromCharCode(code);
+			if(!check){
+				result += ch	
+			}else{
+				result += tab[i]
+			}
+		}
+		if(regexVowels.test(tab[i])){
+			var code = tab[i].charCodeAt() - 5
+			var ch = String.fromCharCode(code);
+			if(!check){
+				result += ch	
+			}else{
+				result += tab[i]
+			}
+		}
+	}
+	return result
+}
+function check(ch){
+	return (ch=== "c" ||ch=== "o" ||ch=== "d" ||ch=== "e")
 }
