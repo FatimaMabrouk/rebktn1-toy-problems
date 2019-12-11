d070779f52ac26c8deb2d013da9afdffe710db73
Abdlrrahman Shibani <56532938+Abdlrrahman@users.noreply.github.com>
2019-12-11 09:34:31 +0100

    13

--- mergeTwoBinaryTrees/merge.js
+++ mergeTwoBinaryTrees/merge.js
@@ -36,55 +36,6 @@ Note: The merging process must start from the root nodes of both trees.
  * @param {TreeNode} t2
  * @return {TreeNode}
  */
- function TreeNode(val) {
-     this.val = val;
-     this.left = this.right = null;
- }
- TreeNode.prototype.addChild = function(child) {
-  if (!child || !(child instanceof TreeNode)) {
-    child = new TreeNode(child);
-  }
-
-  if (!this.isDescendant(child)) {
-    this.children.push(child);
-  } else {
-    throw new Error('That child is already a child of this tree');
-  }
-  // return the new child node for convenience
-  return child;
-};
-
-/**
-  * check to see if the provided tree is already a child of this
-  * tree __or any of its sub trees__
-  */
-TreeNode.prototype.isDescendant = function(child) {
-  if (this.children.indexOf(child) !== -1) {
-    // `child` is an immediate child of this tree
-    return true;
-  } else {
-    for (var i = 0; i < this.children.length; i++) {
-      if (this.children[i].isDescendant(child)) {
-        // `child` is descendant of this tree
-        return true;
-      }
-    }
-    return false;
-  }
-};
-
-/**
-  * remove an immediate child
-  */
-TreeNode.prototype.removeChild = function(child) {
-  var index = this.children.indexOf(child);
-  if (index !== -1) {
-    // remove the child
-    this.children.splice(index, 1);
-  } else {
-    throw new Error('That node is not an immediate child of this tree');
-  }
-};
 var mergeTrees = function(t1, t2) {
 	var newTree = newTree || new TreeNode(0);
 	if (t1.value === null || t2.value === null) {
