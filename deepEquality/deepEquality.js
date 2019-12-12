129fae35355d0b0a5225d9406948ef009ba5192e
Amera Ayad <amera.ayad.it@gmail.com>
2019-12-12 09:47:18 +0100

    need more test

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -11,5 +11,43 @@
   * don't worry about handling cyclical object structures.
   *
   */
-var deepEquals = function(apple, orange) {
-};
+
+//sullotion 1
+var deepEquals = function(apple, orange, result = true) {
+  var appleKeys  = Object.keys(apple);
+  var orangeKeys = Object.keys(orange);
+
+  if(appleKeys.length !== orangeKeys.length){
+    return false;
+  }
+
+  for(var i = 0; i < appleKeys.length; i++){
+    if(apple[appleKeys[i]] === orange[appleKeys[i]]){
+      result = (apple[appleKeys[i]] === orange[appleKeys[i]])  && true;
+    } else {
+      if(typeof apple[appleKeys[i]] === 'object'){
+        return deepEquals(apple[appleKeys[i]], orange[appleKeys[i]], result);
+      } else {
+        return false;
+      }
+    }
+  }
+  return result;
+}
+
+//sollution 2
+var deepEquals = function(apple, orange, o1 = undefined, o2 = undefined) {
+
+  if (o1 && o1 === o2) return true;
+  if (o1 && o1 !== o2) return false;
+  for(var key in apple){
+    if(!deepEquals(apple, orange, apple[key], orange[key]) && typeof apple[key] !=='object'){
+      return false;
+    }
+    if(typeof apple[key] === 'object'){
+      console.log(apple[key])
+      return deepEquals(apple[key], orange[key])
+    }
+  }
+  return true;
+}
