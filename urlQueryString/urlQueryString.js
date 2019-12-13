90a0c0337711709da6c40c6f304f882886fc493d
Anes <marzoukianas@gmail.com>
2019-12-13 09:38:03 +0100

    ...

--- urlQueryString/urlQueryString.js
+++ urlQueryString/urlQueryString.js
@@ -5,8 +5,48 @@ Given a properly formatted URL, return the query string data as an array of key-
 HINT: You may use decodeURIComponent()
 
 parseQueryString("http://example.com?a=hello&b=99") // [ [ "a", "hello" ], [ "b", "99" ] ]
+
 parseQueryString("http://example.com?msg=hello%20world") // [ [ "msg", "hello world" ] ]
+
 parseQueryString("http://example.com") // undefined
 */
+console.log('a=hello&b=99'.split(''))
 
-function parseQueryString(url) {}
+
+
+function parseQueryString(url) { //"http://example.com?a=hello&b=99"
+
+	var dec = decodeURIComponent(url); //http://example.com?a=hello&b=99
+	dec.split('?'); //[ 'http://example.com', 'a=hello&b=99' ]
+	dec = dec[1].split('');
+
+	if(dec.includes('%')) {
+		dec.splice(dec.indexOf('%'),1);
+	}
+		if(dec.includes('%')) {
+		dec.splice(dec.indexOf('%'),1);
+	}
+/*[
+  'a', '=', 'h', 'e',
+  'l', 'l', 'o',
+  'b', '=', '9', '9'
+]
+*/
+var result = [];
+var eql = 0;
+for(var i = 0; i < dec.length; i++) {
+	
+	if(dec[i] === '='){
+		eql ++;
+	}
+	if(dec[i] !== '=') {
+		array.push()
+	}
+}
+
+
+
+
+}
+
+
