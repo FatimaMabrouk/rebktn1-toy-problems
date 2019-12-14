a0577587dd8a7b6868d69e02f717b9988d666926
bilel <hb.bilel@gmail.com>
2019-12-14 09:22:02 +0100

    done git remote -v

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -7,4 +7,15 @@ largestProductOfThree([ 0, 2, 3 ]) //	0
 largestProductOfThree([1, 4, -5, -2]) // 40
 */
 
-function largestProductOfThree(array) {}
+
+function largestProductOfThree(array) {
+    let result = 1
+    array.sort((a,b)=> b-a)
+    for (let i = 0; i < 3; i++){
+        if (array[i] === 0) {
+            return 0
+        }
+        result *= Math.abs(array[i])
+    }
+    return result
+}
