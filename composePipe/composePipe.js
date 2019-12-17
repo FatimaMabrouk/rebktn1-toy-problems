c7533d83d94d7ca9fcb361f9d05f935a8105796f
Anes <marzoukianas@gmail.com>
2019-12-17 09:18:04 +0100

    12/17_toy_problem

--- composePipe/composePipe.js
+++ composePipe/composePipe.js
@@ -15,8 +15,21 @@
  *   var exclaim = function(statement) { return statement.toUpperCase() + '!';}
  *   var welcome = compose(greet, exclaim);
  *   welcome('phillip'); // 'hi: PHILLIP!'
- *
- * Step 2: Implement the function Pipe:
+ */
+'use strict';
+
+
+  // exclaim(greet(name))
+var compose = function(functions) {
+	var arrayOfFunctions = Array.from(arguments); //[function () {console.log('hi') }, function() {console.log('there') }]
+	for(var i =0; i < arrayOfFunctions.length; i++) {
+		//console.log(arrayOfFunctions[i]('philip'))
+		return arrayOfFunctions[i+1]( arrayOfFunctions[i]() );
+	}
+};
+
+
+/* Step 2: Implement the function Pipe:
  *
  * Pipe composes a series of functions and returns the resulting function.
  * 
@@ -29,12 +42,19 @@
  *  var multiplyBy3 = function(number){ return number * 3; }
  *  pipe(add2, multiplyBy3)(5) // 21
  *  pipe(add2, multiplyBy3, multiplyBy3)(5) // 63
+
+ add2( multiplyBy3(number) )
  */
 
-'use strict';
-
-var compose = function() {
+var pipe = function(arg) {
+	var arrayOfFunctions = Array.from(arguments); 
+	for(var i = 0; i < arrayOfFunctions.length; i++) {
+		return arrayOfFunctions[i]( arrayOfFunctions[i+1]() );
+	}
 };
 
-var pipe = function() {
-};
+
+var add2 = function(number){ return number + 2; }
+   var multiplyBy3 = function(number){ return number * 3; }
+   // pipe(add2, multiplyBy3)(5) // 21
+   pipe(add2, multiplyBy3, multiplyBy3)(5) // 63
