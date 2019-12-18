c15f3dba89e6e3c8849512d3052440cf07d93eb1
B.Maher <bouzid.meher@gmail.com>
2019-12-18 09:08:01 +0100

    done

--- treeBFSelect/treeBFSelect.js
+++ treeBFSelect/treeBFSelect.js
@@ -30,15 +30,22 @@
  * Basic tree that stores a value.
  */
 
-var Tree = function(value) {
+var Tree = function (value) {
   this.value = value;
   this.children = [];
 };
 
 
 
-Tree.prototype.BFSelect = function(filter) {
+Tree.prototype.BFSelect = function (filter, result = [], depth = 0) {
   // return an array of values for which the function filter(value, depth) returns true
+  if (filter(this.value, depth)) {
+    result.push(this.value)
+  }
+  for (let i = 0; i < this.children.length; i++) {
+    this.children[i].BFSelect(filter, result, depth + 1)
+  }
+  return result
 };
 
 /**
@@ -49,7 +56,7 @@ Tree.prototype.BFSelect = function(filter) {
   * add an immediate child
   * (wrap values in Tree nodes if they're not already)
   */
-Tree.prototype.addChild = function(child) {
+Tree.prototype.addChild = function (child) {
   if (!child || !(child instanceof Tree)) {
     child = new Tree(child);
   }
@@ -67,7 +74,7 @@ Tree.prototype.addChild = function(child) {
   * check to see if the provided tree is already a child of this
   * tree __or any of its sub trees__
   */
-Tree.prototype.isDescendant = function(child) {
+Tree.prototype.isDescendant = function (child) {
   if (this.children.indexOf(child) !== -1) {
     // `child` is an immediate child of this tree
     return true;
@@ -85,7 +92,7 @@ Tree.prototype.isDescendant = function(child) {
 /**
   * remove an immediate child
   */
-Tree.prototype.removeChild = function(child) {
+Tree.prototype.removeChild = function (child) {
   var index = this.children.indexOf(child);
   if (index !== -1) {
     // remove the child
