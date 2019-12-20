0b95a7f860c2fef9d4482e78dbed4b429f2db59f
B.Maher <bouzid.meher@gmail.com>
2019-12-20 09:17:43 +0100

    done

--- balancedParenthese/balancedParentheses.js
+++ balancedParenthese/balancedParentheses.js
@@ -8,4 +8,14 @@ isBalanced("(50)(")	// false
 isBalanced("") //	true
 */
 
-var isBalanced = function(str) {};
+var isBalanced = function (str) {
+    let rightRegex = new RegExp(/[)]/g);
+    let leftRegex = new RegExp(/[(]/g);
+    let count = 0;
+    if (str[0].match(rightRegex) || str[str.length - 1].match(leftRegex)) return false
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].match(rightRegex)) count--;
+        if (str[i].match(leftRegex)) count++;
+    }
+    return count === 0;
+};
