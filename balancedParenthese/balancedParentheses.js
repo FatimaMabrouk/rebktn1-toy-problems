bdee11dd32fb2dc530c5540b9135cf9dc0ff88eb
Amera Ayad <amera.ayad.it@gmail.com>
2019-12-21 09:35:15 +0100

    refactored, one more if condition

--- balancedParenthese/balancedParentheses.js
+++ balancedParenthese/balancedParentheses.js
@@ -13,6 +13,7 @@ var isBalanced = function(str) {
     for(var i = 0; i < str.length; i++){
         if(str[i] === '(') ++counter;
         if(str[i] === ')') --counter;
+        if(counter < 0) return false;
     }
     return !counter;
 };
