7135f6590d7f0174dd0726014929a2b292026737
waj949 <wajdihamrouni1993@gmail.com>
2019-12-21 09:40:42 +0100

    asyncMAp

--- asyncMap/asyncMap.js
+++ asyncMap/asyncMap.js
@@ -39,4 +39,39 @@
 
 
 var asyncMap = function(tasks, callback) {
+    var queue = [];
+    var result = [];
+    var isAvailable = true;
+    for(var fn of tasks){
+        if(isAvailable){
+        //while fn is still running set IsAvailable to false;
+        while(fn()){
+            isAvailable = false;
+        }
+        queue.push(fn)
+        isAvailable = true;
+      }
+    }
+   for(fn of queue){
+       result.push(callback(fn()))
+   }
+   return result;
 };
+
+ asyncMap([
+      function(cb){
+        setTimeout(function(){
+          cb('one');
+        }, 200);
+      },
+      function(cb){
+        setTimeout(function(){
+         cb('two');
+        }, 100);
+      }
+     ],
+      function(results){
+        // the results array will equal ['one','two'] even though
+        // the second function had a shorter timeout.
+        console.log(results); // ['one', 'two']
+     });
