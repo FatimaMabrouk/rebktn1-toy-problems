c48f87b39e20174df05dee8b46da8c3c048863bf
AbdoAlabed <henry_3030@icloud.com>
2019-12-25 00:21:13 -0800

    done!

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,8 +17,24 @@
 * rockPaperScissors(5); // => ['RRRRR', 'RRRRP', 'RRRRS', etc...]
 *
 */
+//
+var rockPaperScissors = function (numberOfRounds)
+ {
+    var arr = [];
+    var roundGame = Math.floor(Math.random() * 10)+ 1;
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
-};
+    for (var i = 0; i < roundGame; i++) {
+        var sequence = '';
+        
+        for(var j = 0; j < numberOfRounds; j++){
+         var result = Math.floor(Math.random() * 3);
+         switch(result){
+            case 0: sequence+='R'; break;
+            case 1: sequence+='P'; break;
+            case 2: sequence+='S'; break;
+          }   
+        }
+        arr.push(sequence);
+    }
+    return arr
+  };
