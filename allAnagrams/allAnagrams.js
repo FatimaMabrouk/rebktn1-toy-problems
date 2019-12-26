6ff808708c7216d08f43b68afd2345c7e6a06f2e
Anes Marzooki <marzoukianas@gmail.com>
2019-12-26 09:34:57 +0100

    works

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -1,3 +1,5 @@
+
+
 /**
  * Given a single input string, write a function that produces all possible anagrams
  * of a string and outputs them as an array. At first, don't worry about
@@ -12,6 +14,46 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
+
+//lenght * length -1
+
+//(ab) 2 
+//(abc) 6
+//(abcd) 24 
+//factorial string.length IS the limit
+
+
 var allAnagrams = function(string) {
-  // Your code here.
+
+	//======Recursion Function======\\
+	function factorial(n) {
+		if(n === 0) return 1;
+		return n * factorial(n -1); 
+	}
+
+	var limit = factorial(string.length);
+	var obj = {}; //for unique values
+
+	while(Object.keys(obj).length !== limit) {
+		var anag = '';
+		for(var i = 0; i < string.length; i++) {
+			anag += string[Math.floor(Math.random() * 3)];
+		}
+		obj[anag] = anag;//to get unique values.
+	}
+	console.log(Object.keys(obj));
 };
+
+allAnagrams('abcd')
+/*
+[
+  'cabc', 'caac', 'cbcb',
+  'bbac', 'caaa', 'cbaa',
+  'acac', 'ccbb', 'cbcc',
+  'bcaa', 'bcab', 'cccb',
+  'acbb', 'baac', 'baab',
+  'baca', 'cbca', 'bbab',
+  'acab', 'bbca', 'bbcb',
+  'bacb', 'caab', 'cbac'
+]
+*/
